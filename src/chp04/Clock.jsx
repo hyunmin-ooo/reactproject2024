6e1c941497b25abe97bdf6e6a9aa794c0336a8a0
hyunmin-ooo <2201110357@office.kopo.ac.kr>
2024-09-05 16:21:33 +0900

    React 2장, 3장

---
+++ src/chp04/Clock.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Clock(props){
+    return(
+        <div>
+            <h1>안녕, 리액트!</h1>
+            <div className={"cl"}>현재 시간: {new Date().toLocaleTimeString()}</div>
+        </div>
+    );
+}
+export default Clock;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './chp03/Book.css'
+import './chp04/Clock.css';
 import reportWebVitals from './reportWebVitals';
-import Library from "./chp03/Library";
+import ConfirmDialog from "./chp04/ConfirmDialog";
+import Clock from "./chp04/Clock";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>
-);
+
+setInterval(() =>{
+    root.render(
+        <React.StrictMode>
+            <Clock />
+        </React.StrictMode>
+    );
+},1000
+)
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
