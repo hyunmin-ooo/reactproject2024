f19c37305fe89d206925d3d9df498485ae304114
hyunmin-ooo <2201110357@office.kopo.ac.kr>
2024-11-14 14:53:38 +0900

    10장 숫자리스트, 학생출석부1

---
+++ src/chp10/AppStuendtAttendance.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import StudentAttendance from "./StudentAttendance";
+
+function AppStuendtAttendance(){
+    return(
+        <div>
+            <StudentAttendance/>
+        </div>
+    )
+}
+export default AppStuendtAttendance;
--- src/index.js
+++ src/index.js
@@ -21,12 +21,14 @@ import LoginControl from "./chp09/LoginControl";
 import MailBox from "./chp09/MailBox";
 import LoginControl2 from "./chp09/LoginControl2";
 import LandingPage from "./chp09/LandingPage";
+import AppNumberList from "./chp10/AppNumberList";
+import StudentAttendance from "./chp10/StudentAttendance";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 setInterval(() =>{
     root.render(
         <React.StrictMode>
-            <LandingPage/>
+            <StudentAttendance/>
         </React.StrictMode>
     );
 },1000
